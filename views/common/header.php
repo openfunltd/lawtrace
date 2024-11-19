f93f9a6182518de5c6d0cf56131b91e38e0e5d44
typingmonk <tmonk.hsieh@tmonk.tw>
2024-11-19 15:38:30 +0800

    Add basic frontend to index page with search input

--- views/common/header.php
+++ views/common/header.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 <title><?= $this->escape($this->_data['title'] ?? $this->app_name) ?></title>
+<link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
--- views/index/index.php
+++ views/index/index.php
@@ -1,3 +1,19 @@
 <?= $this->partial('common/header', ['title' => 'Lawtrace 首頁']) ?>
-This is Index page
+<div class="container bg-light bg-gradient my-5 rounded-3">
+  <div class="row p-5">
+    <div class="col-6 p-4">
+      <p class="display-5">
+      你想暸解什麼樣的法規沿革或法案討論現狀？
+      </p>
+    </div>
+    <div class="col-6 bg-white rounded-4">
+      <div class="p-5">
+        <div class="input-group">
+          <span class="input-group-text material-symbols-outlined">search</span>
+          <input type="text" class="form-control">
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 <?= $this->partial('common/footer') ?>
