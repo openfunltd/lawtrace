50cf10de34a2849cf8bf524719903c702874191c
typingmonk <tmonk.hsieh@tmonk.tw>
2024-11-19 14:35:40 +0800

    Add default navbar

--- views/common/header.php
+++ views/common/header.php
@@ -5,3 +5,8 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
+<nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">Lawtrace</a>
+  </div>
+</nav>
