e348cb7bf088b8b5ad06f12b5ac7eeeff8bba1ca
typingmonk <tmonk.hsieh@tmonk.tw>
2024-11-22 10:08:06 +0800

    Display aliases and version dropdown on /law/show/{law_id}

--- views/law/show.php
+++ views/law/show.php
@@ -2,12 +2,44 @@
 $law_id = $this->law_id;
 $res = LYAPI::apiQuery("/law/{$law_id}" ,"查詢法律編號：{$law_id} ");
 $law = $res->data ?? new stdClass();
+$aliases = $law->其他名稱 ?? [];
+$res = LYAPI::apiQuery("/law/{$law_id}/versions", "查詢 {$law->名稱} 各法律版本");
+$versions = $res->lawversions ?? [];
+//order by date DESC
+usort($versions, function($v1, $v2) {
+    $date_v1 = $v1->日期 ?? '';
+    $date_v2 = $v2->日期 ?? '';
+    return $date_v2 <=> $date_v1;
+});
 ?>
 <?= $this->partial('common/header', ['title' => 'Lawtrace 搜尋']) ?>
 <div class="container bg-light bg-gradient my-5 rounded-3">
   <div class="row p-5">
     <div class="p-4">
       <h1 class="fw-bold display-6"><?= $this->escape($law->名稱 ?? '') ?></h1>
+      <?php if (!empty($aliases)) { ?>
+        <p class="mt-2 mb-0 fs-5">
+          別名：
+          <?= $this->escape(implode('、', $aliases)) ?>
+        </p>
+      <?php } ?>
+      <div class="mt-3 mb-0 fs-5 btn-group">
+        <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+          版本：<?= $this->escape("{$versions[0]->日期} {$versions[0]->動作}") ?>
+        </button>
+        <ul class="dropdown-menu">
+          <?php foreach ($versions as $version) { ?>
+            <li>
+              <a
+                class="dropdown-item"
+                href="/law/show/<?= $this->escape($law_id) ?>?version=<?= $this->escape($version->版本編號) ?>"
+              >
+                <?= $this->escape("{$version->日期} {$version->動作}") ?>
+              </a>
+            </li>
+          <?php } ?>
+        </ul>
+      </div>
     </div>
   </div>
 </div>
