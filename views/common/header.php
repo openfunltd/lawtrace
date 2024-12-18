f618051c9709f5088fa59108b4c901d9b8a3acc7
typingmonk <tmonk.hsieh@tmonk.tw>
2024-12-18 09:31:02 +0800

    Initial integrate with lawtrace-frontend

--- views/common/header.php
+++ views/common/header.php
@@ -1,13 +1,55 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<title><?= $this->escape($this->_data['title'] ?? $this->app_name) ?></title>
-<link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?= $this->escape($this->_data['title'] ?? $this->app_name) ?></title>
+  <meta name=description content="">
+  <meta name="og:type" content="website">
+  <meta name="og:title" content="">
+  <meta name="og:description" content="">
+  <meta name="og:site_name" content="">
+  <meta name="og:image" content="">
+  <link rel="shortcut icon" href="favicon.ico">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100..900&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="/static/css/common.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.bundle.min.js" integrity="sha512-7Pi/otdlbbCR+LnW+F7PwFcSDJOuUJB3OxtEHbg4vSMvzvJjde4Po1v4BR9Gdc9aXNUNFVUY+SK51wWT8WF0Gg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </head>
 <body>
-<nav class="navbar bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="/">Lawtrace</a>
-  </div>
-</nav>
+  <div class="wrapper">
+    <nav class="lt-navbar">
+      <a class="logo" href="/">
+        LawTrace
+      </a>
+      
+      <div class="dropdown">
+        <a href="#" class="lt-nav-items" data-bs-toggle="dropdown">
+          關於
+          <i class="fa fa-chevron-down dropdown-icon"></i>
+        </a>
+        <ul class="dropdown-menu">
+          <li>
+            <a class="dropdown-item" href="#">
+              Menu Item 1
+            </a>
+          </li>
+          <li>
+            <a class="dropdown-item" href="#">
+              Menu Item 2
+            </a>
+          </li>
+          <li>
+            <hr class="dropdown-divider">
+          </li>
+          <li>
+            <a class="dropdown-item" href="#">
+              Menu Item 3
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
--- views/index/index.php
+++ views/index/index.php
@@ -1,20 +1,87 @@
 <?= $this->partial('common/header', ['title' => 'Lawtrace 首頁']) ?>
-<div class="container bg-light bg-gradient my-5 rounded-3">
-  <div class="row p-5">
-    <div class="col-6 p-4">
-      <p class="display-5">
-      你想暸解什麼樣的法規沿革或法案討論現狀？
-      </p>
-    </div>
-    <div class="col-6 bg-white rounded-4">
+<div class="main">
+  <section class="search-form">
+    <div class="container">
       <form action="/search" method="get">
-        <div class="p-5">
-          <div class="input-group">
-            <span class="input-group-text material-symbols-outlined">search</span>
-            <input type="text" class="form-control" placeholder="請輸入關鍵字" name="q" required>
-          </div>
+        <div class="brand-name">
+          LawTrace
+        </div>
+        <h2>
+          你想暸解什麼法律的沿革或討論現狀？
+        </h2>
+        <div>
+          <input type="search" class="form-control search-input" placeholder="關鍵字" name="q" required>
+        </div>
+        <div class="text-end">
+          <button type="submit" class="btn btn-primary">
+            搜尋
+          </button>
         </div>
       </form>
+    </div>
+  </section>
+
+  <div class="main-content">
+    <div class="container about-law-trace">
+      <section class="concept">
+        <h2 class="title">
+          一站式地查詢法律相關資料
+        </h2>
+        <div class="desc">
+          法律相關的資料時常散落在各處，找起來挺不簡單的，所以 LawTrace 將資訊彙整在一處，讓找尋資料時，能少開幾個分頁、更有效率。祝查找愉快！
+        </div>
+      </section>
+
+      <section class="features">
+        <div class="feature-item">
+          <div class="icon">
+            <img src="static/images/feature1.svg" alt="瀏覽各版本的法律內容">
+          </div>
+          <div class="title">
+            瀏覽各版本的法律內容
+          </div>
+          <div class="desc">
+            從新增條文到後續所有修正版本，均可一覽無遺，快速掌握條文的演變過程。
+          </div>
+        </div>
+
+        <div class="feature-item">
+          <div class="icon">
+            <img src="static/images/feature2.svg" alt="查看修訂歷程">
+          </div>
+          <div class="title">
+            查看修訂歷程
+          </div>
+          <div class="desc">
+            一覽法律/法條修訂過程中變更的細節，以及經歷的流程，如：一二三讀、審查會議、公聽會等等。
+          </div>
+        </div>
+
+        <div class="feature-item">
+          <div class="icon">
+            <img src="static/images/feature3.svg" alt="比較相關議案/條文">
+          </div>
+          <div class="title">
+            比較相關議案/條文
+          </div>
+          <div class="desc">
+            將各議案中的條文草案或不同版本的條文並排呈現，比較差異。
+          </div>
+        </div>
+      </section>
+
+      <section class="refer">
+        <h3 class="title">
+          LawTrace 的資料來源
+        </h3>
+        <ul>
+          <li>agsadfgahadhah</li>
+          <li>agsadfgahadhah</li>
+          <li>agsadfgahadhah</li>
+          <li>agsadfgahadhah</li>
+          <li>agsadfgahadhah</li>
+        </ul>
+      </section>
     </div>
   </div>
 </div>
