9177cf593bbbac9e054be0adda157832b01864bb
typingmonk <tmonk.hsieh@tmonk.tw>
2025-01-09 10:42:04 +0800

    Initial merge frontend to /law/history

--- views/law/history.php
+++ views/law/history.php
@@ -1,4 +1,116 @@
 <?php
+$law_id = $this->law_id;
+$version_id_input = $this->version_id_input;
+
+if (! ctype_digit($law_id)) {
+    header('HTTP/1.1 400 Bad Request');
+    echo "<h1>400 Bad Request</h1>";
+    echo "<p>Invalid law_id</p>";
+    exit;
+}
+
+$res = LYAPI::apiQuery("/law/{$law_id}" ,"查詢法律編號：{$law_id} ");
+$res_error = $res->error ?? true;
+if ($res_error) {
+    header('HTTP/1.1 404 No Found');
+    echo "<h1>404 No Found</h1>";
+    echo "<p>No law data with law_id {$law_id}</p>";
+    exit;
+}
+$law = $res->data;
+$aliases = $law->其他名稱 ?? [];
+$vernaculars = $law->別名 ?? [];
+
+$res = LYAPI::apiQuery("/law/{$law_id}/versions", "查詢 {$law->名稱} 各法律版本");
+$versions = $res->lawversions ?? [];
+if ($version_id_input != 'latest') {
+    $invalid_version = true;
+    foreach ($versions as $version) {
+        $version_id = $version->版本編號 ?? NULL;
+        if ($version_id_input == $version_id) {
+            $invalid_version = false;
+            $version_id_selected = $version_id;
+            $version_selected = $version;
+            break;
+        }
+    }
+    if ($invalid_version) {
+        header('HTTP/1.1 404 No Found');
+        echo "<h1>404 No Found</h1>";
+        echo "<p>No version data with version_id {$version_id_input}</p>";
+        exit;
+    }
+}
+
+//versions order by date DESC
+usort($versions, function($v1, $v2) {
+    $date_v1 = $v1->日期 ?? '';
+    $date_v2 = $v2->日期 ?? '';
+    return $date_v2 <=> $date_v1;
+});
+if ($version_id_input == 'latest') {
+    foreach ($versions as $version) {
+        $version_id = $version->版本編號 ?? NULL;
+        if (isset($version_id)) {
+            $version_id_selected = $version_id;
+            $version_selected = $version;
+            break;
+        }
+    }
+}
+$histories = $version_selected->歷程 ?? [];
+$histories = array_reverse($histories);
+//histories order by date DESC
+usort($histories, function($h1, $h2) {
+    $date_h1 = $h1->會議日期 ?? '';
+    $date_h2 = $h2->會議日期 ?? '';
+    return $date_h2 <=> $date_h1;
+});
+$show_endpoint = "/law/show/{$law_id}";
+if ($version_id_input != 'latest') {
+    $show_endpoint = $show_endpoint . "?version={$version_id_input}";
+}
 ?>
 <?= $this->partial('common/header', ['title' => '法律內容']) ?>
+<div class="main">
+  <section class="page-hero law-details-info">
+    <div class="container">
+      <nav class="breadcrumb-wrapper">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item">
+            <a href="index.html">
+              <i class="bi bi-house-door"></i>
+            </a>
+          </li>
+          <li class="breadcrumb-item active">
+            法律資訊
+          </li>
+        </ol>
+      </nav>
+      <h2 class="light">
+        <?= $this->escape($law->名稱 ?? '') ?>
+      </h2>
+      <div class="info">
+        <?php if (!empty($aliases)) { ?>
+          <div class="alias">
+            別名：<?= $this->escape(implode('、', $aliases)) ?>
+          </div>
+        <?php } ?>
+        <?php if (!empty($vernaculars)) { ?>
+          <div class="vernacular">
+            俗名：<?= $this->escape(implode('、', $vernaculars)) ?>
+          </div>
+        <?php } ?>
+      </div>
+      <div class="btn-group law-pages">
+        <a href="<?= $this->escape($show_endpoint) ?>" class="btn btn-outline-primary">
+          瀏覽法律
+        </a>
+        <a href="#" class="btn btn-outline-primary active">
+          查看修訂歷程
+        </a>
+      </div>
+    </div>
+  </section>
+</div>
 <?= $this->partial('common/footer') ?>
