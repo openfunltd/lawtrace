c4a4d01743a990f1f8266347751cd542d8436b97
typingmonk <tmonk.hsieh@tmonk.tw>
2025-01-10 16:27:02 +0800

    Add party .svg files and PartyHelper

---
+++ libraries/PartyHelper.php
@@ -0,0 +1,23 @@
+<?php
+
+class PartyHelper
+{
+    public static $icon_files = [
+        '民主進步黨' => '/static/images/party/dpp.svg',
+        '民進黨' => '/static/images/party/dpp.svg',
+        '中國國民黨' => '/static/images/party/kmt.svg',
+        '國民黨' => '/static/images/party/kmt.svg',
+        '勞動黨' => '/static/images/party/lp.svg',
+        '無黨籍' => '/static/images/party/no_party.svg',
+        '無黨團結聯盟' => '/static/images/party/non.svg',
+        '新黨' => '/static/images/party/np.svg',
+        '親民黨' => '/static/images/party/pfp.svg',
+        '社會民主黨' => '/static/images/party/sd.svg',
+        '台灣民眾黨' => '/static/images/party/tpp.svg',
+        '台灣基進' => '/static/images/party/tsp.svg',
+        '台灣團結聯盟' => '/static/images/party/tsu.svg',
+        '正神名黨' => '/static/images/party/zsm.svg',
+        '時代力量' => '/static/images/party/tnpp.svg',
+        'none' => '/static/images/party/none.svg',
+    ];
+}
