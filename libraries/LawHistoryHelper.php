9903e36534841ed28ec11258c6b7c52d1b8aed81
typingmonk <tmonk.hsieh@tmonk.tw>
2025-01-11 07:46:04 +0800

    Refactor LawHistoryHelper::getDetailedHistories via guard clause

--- libraries/LawHistoryHelper.php
+++ libraries/LawHistoryHelper.php
@@ -16,35 +16,44 @@ class LawHistoryHelper
             $related_doc = $related_doc[0] ?? new stdClass();
             $bill_id = $related_doc->billNo ?? null;
             $history->proposer_or_progress = $history->進度;
+
             if (isset($bill_id)) {
                 $res = LYAPI::apiQuery("/bill/{$bill_id}","查詢提案詳細資訊 bill_id: {$bill_id}");
                 $res_error = $res->error ?? true;
                 if (!$res_error) {
                     $history->bill_id = $bill_id;
                     $bill = $res->data;
-                    $proposer = $bill->{'提案單位/提案委員'} ?? '';
-                    $proposer = self::trimProposer($proposer);
-                    $history->proposer_or_progress = $proposer;
-                    $party_img_path = PartyHelper::getImage($proposer);
-                    $term = $bill->屆 ?? 0;
-                    $proposers = $bill->提案人 ?? [];
-                    $leading_proposer = $proposers[0] ?? NULL;
-                    if (is_null($party_img_path) and $term != 0 and isset($leading_proposer)) {
-                        $res = LYAPI::apiQuery(
-                            "/legislator/{$term}-{$leading_proposer}",
-                            "查詢 {$term}-{$leading_proposer} 黨籍"
-                        );
-                        $res_error = $res->error ?? true;
-                        if (!$res_error) {
-                            $legislator = $res->data;
-                            $party = $legislator->黨籍;
-                            $party_img_path = PartyHelper::getImage($party);
-                        }
-                    }
-                    if (isset($party_img_path)) {
-                        $history->party_img_path = $party_img_path;
-                    }
                 }
+            }
+
+            if (!isset($bill)) {
+                continue;
+            }
+
+            //get proposer or progress title
+            $proposer = $bill->{'提案單位/提案委員'} ?? '';
+            $proposer = self::trimProposer($proposer);
+            $history->proposer_or_progress = $proposer;
+
+            //determine party image
+            $party_img_path = PartyHelper::getImage($proposer);
+            $term = $bill->屆 ?? 0;
+            $proposers = $bill->提案人 ?? [];
+            $leading_proposer = $proposers[0] ?? NULL;
+            if (is_null($party_img_path) and $term != 0 and isset($leading_proposer)) {
+                $res = LYAPI::apiQuery(
+                    "/legislator/{$term}-{$leading_proposer}",
+                    "查詢 {$term}-{$leading_proposer} 黨籍"
+                );
+                $res_error = $res->error ?? true;
+                if (!$res_error) {
+                    $legislator = $res->data;
+                    $party = $legislator->黨籍;
+                    $party_img_path = PartyHelper::getImage($party);
+                }
+            }
+            if (isset($party_img_path)) {
+                $history->party_img_path = $party_img_path;
             }
         }
 
