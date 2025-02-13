c3dee901cdae356de45d69ec324fb3fc356e34ee
typingmonk <tmonk.hsieh@tmonk.tw>
2025-02-13 16:28:54 +0800

    Update about

--- views/about/index.php
+++ views/about/index.php
@@ -19,7 +19,6 @@
         <div class="desc">
           <p>我們透過研究發現，即使是倡議團體、媒體記者、立委助理、事實查核團體等經常查詢立法院資料的專業工作者，在檢索法案資料時，依舊需要耗費大量時間，跨越多個網站與開啟一長串的分頁，或是埋首在數百頁的文件中才能找到所需資訊。</p>
           <p>對於不常使用立法院網站的一般民眾來說，想查法案資料更是困難重重。資料難以取得，背離立法院公開資料的初衷，公民因此難以關注修法進程，民主精神難以落實。</p>
-          <br>
           <p>完整研究報告可<a href="https://openfun.tw/ly-user-study/" target="_blank">由此下載</a>。</p>
         </div>
       </section>
@@ -28,34 +27,34 @@
           開發團隊
         </h2>
         <div class="desc">
-          <h3>歐噴有限公司</h3>
-          <p>
-            LawTrace 是由
-            <a href="https://openfun.tw/" target="_blank" class="black underline">歐噴有限公司</a>
-            開發的專案。我們希望以技術為橋樑，透過開放資料，打造更加開放與包容的民主社會。
-          </p>
+          <h3><a href="https://openfun.tw/" target="_blank">歐噴有限公司</a></h3>
           <p>歐噴擁有專業的資料爬蟲技術、文字摘要能力，以及處理大型數據、文字、影音資料的豐富經驗。此外，我們擁有國內最頂尖的立法院國會資料處理技術，瞭解利害關係人對立法資訊的需求與痛點。</p>
-          <p>我們更希望，未來立法院能直接將 LawTrace 整合至官方網站，真正落實開放資料的美意，讓全民更方便地檢索法案資訊。</p>
-          <p>歐噴期望 LawTrace 作為促進民主參與的支點，透過開放資料與提升資料易用性，讓立法與修法的過程能被更多人看見、促進基於真實資料的公共議題討論。</p>
-          <h3>西敏寺民主基金會</h3>
-          <p>原文為 Westminster Foundation for democracy（WFD）合作支持開發 LawTrace。</p>
-          <p>WFD是英國的公共機構，致力於協助世界各地的民主加強與深化，打造自由與繁榮的公民社會。</p>
+          <p>歐噴開發 LawTrace ，期望透過提升資料易用性，讓立法與修法的過程能被更多人看見、促進基於真實資料的公共議題討論。</p>
+          <p>我們希望以技術為橋樑，透過開放資料，打造更加開放與包容的民主社會。</p>
+          <p>本網站由<a href="https://www.wfd.org/" target="_blank">西敏寺民主基金會 (Westminster Foundation for Demoracy)</a>支持開發。</p>
         </div>
       </section>
       <section class="concept">
         <h2 class="title">
-          感謝個人（待補）
+          合作夥伴
         </h2>
         <div class="desc">
-          <h3>待補</h3>
+          <p>歐噴與幾位可靠的夥伴合作，一起開發 LawTrace</p>
+          <ul>
+            <li><a href="https://www.linkedin.com/in/claire-cheng-32b991123/" target="_blank">Claire Cheng</a> / 專案經理</li>
+            <li>Sandra Lin / 使用者研究與設計師</li>
+            <li><a href="https://boggy.tw/" target="_blank">Boggy Jan</a> / 前端設計師</li>
+          <ul>
         </div>
       </section>
       <section class="concept">
         <h2 class="title">
-          如果你想參與更多（待補）
+          加入開放國會的行列
         </h2>
         <div class="desc">
-          <h3>待補</h3>
+          <h3>零時政府（g0v） 國會松</h3>
+          <p>這是由一群關心國會透明與開放的公民們自行發起的專案。我們每月舉辦一次活動，，聚焦國會監督、資料治理、介面設計與測試等議題。透過跨界合作，拉近民間與政局距離，深化民主影響力。</p>
+          <p>歡迎<a href="https://g0v-tw.slack.com/archives/CDRE0Q0CE" target="_blank">加入 g0v slack 與我們一起交流</a>（若你尚未註冊 g0v slack 帳號，請先<a href="https://join.g0v.tw/" target="_blank">由此註冊</a>），一起推動改變！</p>
         </div>
       </section>
     </div>
