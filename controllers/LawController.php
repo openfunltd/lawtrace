fb0a4b3068155369db2b557417e11c5a64bcb3dc
typingmonk <tmonk.hsieh@tmonk.tw>
2025-02-20 13:54:33 +0800

    Initial commit of /law/compare

--- controllers/LawController.php
+++ controllers/LawController.php
@@ -33,4 +33,11 @@ class LawController extends MiniEngine_Controller
         $this->view->law_id = $law_id;
         $this->view->version_id_input = $version_id_input;
     }
+
+    public function compareAction()
+    {
+        $source_input = filter_input(INPUT_GET, 'source', FILTER_SANITIZE_SPECIAL_CHARS) ?? Null;
+
+        $this->view->source_input = $source_input;
+    }
 }
---
+++ views/law/compare.php
@@ -0,0 +1,6 @@
+<?php
+$source_input = $this->source_input;
+?>
+<?= $this->partial('common/header', ['title' => "比較議案／條文"]) ?>
+<?= $source_input ?>
+<?= $this->partial('common/footer') ?>
