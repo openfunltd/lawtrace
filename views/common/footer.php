ef46d4d144d484cea3599d7e525c64ccc15efd8a
Ronny Wang <ronnywang@gmail.com>
2025-02-21 14:29:25 +0800

    add debug feature

--- views/common/footer.php
+++ views/common/footer.php
@@ -1,6 +1,13 @@
 <div class="lt-footer">
   <div class="container">
-    footer
+      footer
+      <?php if ($_GET['debug'] ?? false) { ?>
+      <ul>
+          <?php foreach (LYAPI::getLogs() as $log) { ?>
+          <li><?= $this->escape(implode(':', $log)) ?></li>
+          <?php } ?>
+      </ul>
+      <?php } ?>
   </div>
 </div>
 </div>
