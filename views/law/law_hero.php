cdf2f9044290e3075592c8715f38a03429690308
Ronny Wang <ronnywang@gmail.com>
2025-02-25 17:56:06 +0800

    add meet and bill info in law_hero

--- views/law/law_hero.php
+++ views/law/law_hero.php
@@ -38,7 +38,27 @@ if ($this->version_id_input != 'latest') {
               <i class="bi bi-info-circle" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="過往曾在三讀通過的版本中使用的名稱，或是在提案、公文中曾出現的其他稱呼。"></i>
               ：<?= $this->escape(implode('、', $this->aliases)) ?>
           </div>
-        <?php } ?>
+      <?php } ?>
+      <?php if ($this->source_type == 'meet') { ?>
+      <div class="review-committee">
+          審查委員會：<?= $this->escape(implode('、', $this->meet->{'委員會代號:str'})) ?>
+      </div>
+      <div class="review-date">
+          審查會議日期：<?= $this->escape(LawVersionHelper::getMinguoDate($this->meet->日期[0])) ?>
+      </div>
+      <div class="convener">
+        召委：
+        <!--<img src="images/party/tpp.svg">-->
+        <?= $this->escape($this->meet->會議資料[0]->委員會召集委員 ?? '') ?>
+      </div>
+      <?php } elseif ($this->source_type == 'bill') { ?>
+      <div class="review-committee">
+          審查委員會：<?= $this->escape(str_replace('本院', '', $this->bill->{'提案單位/提案委員'})) ?>
+      </div>
+      <div class="review-date">
+          審查會發文日期：<?= $this->escape(LawVersionHelper::getMinguoDate($this->bill->議案流程[0]->日期[0] ?? '')) ?>
+      </div>
+      <?php } ?>
       </div>
       <div class="btn-group law-pages">
           <?php if ('meet' == $this->source_type) { ?>
