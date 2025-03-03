78c7875a39f127443eed10a9999e664e29ae5991
typingmonk <tmonk.hsieh@tmonk.tw>
2025-03-03 09:57:49 +0800

    Remove deprecated codes used for proof of concepts

--- controllers/LawarticleController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class LawarticleController extends MiniEngine_Controller
-{
-    public function showAction($law_content_id)
-    {
-        $this->view->law_content_id = $law_content_id;
-    }
-}
--- controllers/LawdiffController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class LawdiffController extends MiniEngine_Controller
-{
-    public function showAction($bill_no)
-    {
-        $this->view->bill_no = $bill_no;
-    }
-}
