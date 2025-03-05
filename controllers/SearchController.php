7514b79f2af6e1fc035b44943a5a3f83b61078f5
Ronny Wang <ronnywang@gmail.com>
2025-03-05 12:04:53 +0800

    add feature to search billNo to redirect compare page

--- controllers/SearchController.php
+++ controllers/SearchController.php
@@ -5,5 +5,9 @@ class SearchController extends MiniEngine_Controller
     public function indexAction()
     {
         $this->view->q = filter_input(INPUT_GET, 'q', FILTER_SANITIZE_STRING) ?? '';
+        // billNo 直接轉到條文比較工具
+        if (preg_match('#^\d+$#', $this->view->q) and strlen($this->view->q) > 10) {
+            return $this->redirect("/law/compare?source=bill:" . $this->view->q);
+        }
     }
 }
