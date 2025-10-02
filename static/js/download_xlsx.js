4747b379e2a20a5921612a2d698c001a9b786bbe
typingmonk <tmonk.hsieh@tmonk.tw>
2025-10-02 15:08:32 +0800

    Collect titles of each bill #28

--- static/js/download_xlsx.js
+++ static/js/download_xlsx.js
@@ -1,3 +1,9 @@
 $("#download-xlsx").on("click", function() {
-    //TODO
+  let data = [];
+  let titles = ['版本名稱'];
+  $(".law-diff-head").each(function(index, element) {
+    title_div = $(element).find('.title');
+    title_str = title_div.clone().children().remove().end().text().trim();
+    titles.push(title_str);
+  });
 });
