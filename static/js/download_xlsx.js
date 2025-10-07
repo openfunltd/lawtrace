ca844ed519f87662fd7cb7ae099d2fcba7850328
typingmonk <tmonk.hsieh@tmonk.tw>
2025-10-07 11:15:58 +0800

    Add metadata from different kind of source #28

--- static/js/download_xlsx.js
+++ static/js/download_xlsx.js
@@ -80,10 +80,9 @@ $("#download-xlsx").on("click", function() {
   //get law_name, source_str and timestamp for excel file name
   const law_name = $('li.breadcrumb-item').eq(1).find('a').text().trim();
   const source_str = buildSourceStr();
-  const time = getTimestamp();
 
   //downlaod excel file
-  XLSX.writeFile(workbook, `${law_name}-${source_str}-${time}.xlsx`);
+  XLSX.writeFile(workbook, `${law_name}-${source_str}.xlsx`);
 });
 
 function getLawAoa(last_section_idx, articleNums, bill_count, split) {
@@ -179,9 +178,71 @@ function getMetadataAoa() {
   let metadata_aoa = [];
   const source_type_str = $('li.breadcrumb-item').eq(2).text().trim();
   metadata_aoa.push(['Lawtrace 網址', window.location.href]);
-  metadata_aoa.push(['檔案下載時間', getTimestampHumanReadable()]);
+  metadata_aoa.push(['檔案下載時間', getTimestamp()]);
   metadata_aoa.push(['法律名稱', $('li.breadcrumb-item').eq(1).find('a').text().trim()]);
   metadata_aoa.push(['比較來源', source_type_str]);
+
+  //不同 source 的詳細資訊
+  if (source_type_str == '三讀版本') {
+    metadata_aoa.push(['版本', $('li.breadcrumb-item').eq(3).text().trim()]);
+  } else if (source_type_str == '審查報告') {
+    metadata_aoa.push(['審查委員會', $('div.review-committee').first().text().trim().split('：')[1]]);
+    metadata_aoa.push(['審查會發文日期', $('div.review-date').eq(0).text().trim().split('：')[1]]);
+    metadata_aoa.push(['議案狀態', $('div.review-date').eq(1).text().trim().split('：')[1]]);
+  } else if (source_type_str == '法律議案') {
+    divs = $('div.review-committee').toArray();
+
+    //提案人 array
+    let proposers = [];
+    for (let i = 0; i < divs.length; i++) {
+      if ($(divs[i]).text().includes('提案人')) {
+        let imgs = $(divs[i]).find('img').toArray();
+        for (let j = 0; j < imgs.length; j++) {
+          proposers.push($(imgs[j]).attr('alt').trim());
+        }
+      }
+    }
+    const proposer_aoa = ['提案人'].concat(proposers);
+
+    //連署人 array
+    let cosigners = [];
+    for (let i = 0; i < divs.length; i++) {
+      if ($(divs[i]).text().includes('連署人')) {
+        let imgs = $(divs[i]).find('img').toArray();
+        for (let j = 0; j < imgs.length; j++) {
+          cosigners.push($(imgs[j]).attr('alt').trim());
+        }
+      }
+    }
+    const cosigner_aoa = ['連署人'].concat(cosigners);
+
+    //提案單位
+    let proposing_unit_aoa = ['提案單位', ''];
+    for (let i = 0; i < divs.length; i++) {
+      if ($(divs[i]).text().includes('提案單位')) {
+        proposing_unit_aoa = ['提案單位', $(divs[i]).text().split('：')[1].trim()];
+      }
+    }
+
+    //案由
+    let bill_proposal_aoa = ['案由', ''];
+    for (let i = 0; i < divs.length; i++) {
+      if ($(divs[i]).text().includes('案由')) {
+        bill_proposal_aoa = ['案由', $(divs[i]).text().split('：')[1].trim()];
+      }
+    }
+
+    metadata_aoa.push(proposer_aoa);
+    metadata_aoa.push(cosigner_aoa);
+    metadata_aoa.push(proposing_unit_aoa);
+    metadata_aoa.push(['提案日期', $('div.review-date').eq(0).text().trim().split('：')[1]]);
+    metadata_aoa.push(['提案狀態', $('div.review-date').eq(1).text().trim().split('：')[1]]);
+    metadata_aoa.push(bill_proposal_aoa);
+  } else if (source_type_str == '委員會審查') {
+    metadata_aoa.push(['審查委員會', $('div.review-committee').first().text().trim().split('：')[1]]);
+    metadata_aoa.push(['審查會議日期', $('div.review-date').first().text().trim().split('：')[1]]);
+    metadata_aoa.push(['召委', $('div.convener').first().text().trim().split('：')[1]]);
+  }
 
   return metadata_aoa;
 }
@@ -205,18 +266,6 @@ function chunkArray(arr, n) {
 }
 
 function getTimestamp() {
-  const now = new Date();
-  return (
-    now.getFullYear().toString() +
-    String(now.getMonth() + 1).padStart(2, '0') +
-    String(now.getDate()).padStart(2, '0') +
-    String(now.getHours()).padStart(2, '0') +
-    String(now.getMinutes()).padStart(2, '0') +
-    String(now.getSeconds()).padStart(2, '0')
-  );
-}
-
-function getTimestampHumanReadable() {
   const now = new Date();
   return (
     now.getFullYear().toString() + '-' +
