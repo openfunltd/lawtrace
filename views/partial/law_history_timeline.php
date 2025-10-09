5d724d36c12ab5ff582229de0ed6de8b0e7e326c
typingmonk <tmonk.hsieh@tmonk.tw>
2025-10-09 09:03:38 +0800

    Display LYZER link for 進度: 委員會(公聽會) #25

--- views/partial/law_history_timeline.php
+++ views/partial/law_history_timeline.php
@@ -99,7 +99,7 @@ $history_groups = $this->_data['history_groups'];
                 公報
               </div>
               <div class="card-right">
-                <?php if ($history->進度 == '委員會審查' and property_exists($history, 'agenda_id')) { ?>
+                <?php if (mb_strpos($history->進度, '委員會') !== false and property_exists($history, 'agenda_id')) { ?>
                 <a class="btn btn-sm btn-outline-primary"
                   href="https://wtocabqhfbuwyhwhhdcg.supabase.co/functions/v1/find-by-agenda-id?agenda_id=<?= $history->agenda_id ?>"
                   target="_blank"
