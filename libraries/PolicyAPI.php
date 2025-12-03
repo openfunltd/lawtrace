9676e61484f7065f35c821a948bc718ab938d7a9
typingmonk <tmonk.hsieh@tmonk.tw>
2025-12-03 14:08:11 +0800

    Fix wrong variable name #36

--- libraries/PolicyAPI.php
+++ libraries/PolicyAPI.php
@@ -34,7 +34,7 @@ class PolicyAPI
         }
 
         $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, $api_url);
+        curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         // referer
         curl_setopt($curl, CURLOPT_REFERER, 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
